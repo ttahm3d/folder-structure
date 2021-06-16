0445ee34e662664b065964f0bdf259eeba173e0f
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2021-06-16 21:29:09 +0530

    feat: added required packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchData = async () => {
+  const response = await axios.get(
+    `https://api.npoint.io/93bed93a99df4c91044e`
+  );
+  return response.data;
+};
 
 function App() {
+  const { data, error, loading } = useQuery("restaurantData", fetchData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error && <p>Error in loading</p>}
+      {loading && <p>Loading</p>}
+      {data && console.log(data)}
+      <h1>Folder structure</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import reportWebVitals from "./reportWebVitals";
+
+const client = new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </QueryClientProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
